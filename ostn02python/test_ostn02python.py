b96410450f4f6ee96840e4a6c2cf8441f5867a6d
Ian Hopkinson <ianhopkinson@googlemail.com>
2016-02-28 13:15:40 +0000

    Add test for OSGB36_to_ETRS89

--- ostn02python/test_ostn02python.py
+++ ostn02python/test_ostn02python.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 from ostn02python.OSGB import parse_grid
+from ostn02python.OSTN02 import OSGB36_to_ETRS89
 from nose.tools import assert_equal
 # from OSTN02 import *
 
@@ -12,3 +13,11 @@ def test_parse_grid1():
 def test_parse_grid2():
     xin, yin = parse_grid("NY", 46200, 75400)
     assert_equal((xin, yin), (346200, 575400))
+
+def test_OSGB36_to_ETRS89():
+    xin = 614300
+    yin = 159900
+
+    (x,y,h) = OSGB36_to_ETRS89(xin, yin)
+
+    assert_equal((614199.522, 159979.837, 44.622), (x, y, h))
