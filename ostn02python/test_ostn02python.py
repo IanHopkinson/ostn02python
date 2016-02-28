8f4dfb1053d4628234f9193193abba0e4b49b475
Ian Hopkinson <ianhopkinson@googlemail.com>
2016-02-28 12:33:36 +0000

    Add tests for parse_grid

---
+++ ostn02python/test_ostn02python.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from ostn02python.OSGB import parse_grid
+from nose.tools import assert_equal
+# from OSTN02 import *
+
+def test_parse_grid1():
+    xin, yin = parse_grid("ST", 00000, 00000)
+    assert_equal((xin, yin), (300000, 100000))
+
+def test_parse_grid2():
+    xin, yin = parse_grid("NY", 46200, 75400)
+    assert_equal((xin, yin), (346200, 575400))
